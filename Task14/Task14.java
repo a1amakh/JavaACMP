856783f7d0f87ca8189a325a9818bffd3b1bae94
a1amakh <akukalevich@gmail.com>
2023-02-28 19:12:38 +0300

    Initial commit

---
+++ Task14/Task14.java
@@ -0,0 +1,44 @@
+/**
+ * Task No14 from acmp.ru
+ */
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+class Task14 {
+	
+
+	public static void main(String[] args) {
+		// If file "INPUT.txt" successfully read - true
+		if (readInputFile())
+			writeOutputFile();
+	}
+
+	
+
+	private static boolean readInputFile() { // Method for read file "INPUT.txt"
+		try (Scanner scanner = new Scanner(new File("INPUT.txt"))) { // Try to open file "INPUT.txt"
+	
+		
+            System.out.println("File \"INPUT.txt\" successfully read"); // Show message when file "INPUT.txt" successfully read
+			return true;
+        } catch (FileNotFoundException e) {
+            System.out.println("File \"INPUT.txt\" not found"); // Show message when file "INPUT.txt" not found
+			return false;
+        }
+	}
+
+	private static boolean writeOutputFile() { // Method for write file "OUTPUT.txt"
+		try (PrintWriter printWriter = new PrintWriter(new File("OUTPUT.txt"))) { // Try to open file "OUTPUT.txt" 
+		
+		
+			System.out.println("File \"OUTPUT.txt\" successfully written"); // Show message when file "OUTPUT.txt" successfully written
+			return true;
+		} catch (FileNotFoundException e) {
+			System.out.println("File \"OUTPUT.txt\" not found"); // Show message when file "OUTPUT.txt" not found
+			return false;
+		}
+	}
+}
